70bf3f553dd400ed6a543ddf52f92b8fada2e657
gldnrt <19574992+gldnrt@users.noreply.github.com>
2022-12-30 06:49:34 +0900

    Add: パラメータファイルの読み込み処理を追加

--- st_cli/__main__.py
+++ st_cli/__main__.py
@@ -1,4 +1,14 @@
+import sys
 from . import cli
 
+
+def main():
+    try:
+        cli.create_signed_transaction(sys.argv)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        exit(1)
+
+
 if __name__ == "__main__":
-    cli.demo()
+    main()
--- st_cli/cli.py
+++ st_cli/cli.py
@@ -1,2 +1,23 @@
-def demo():
-    print("Hello World!")
+import json
+
+
+def get_params(argv):
+    if len(argv) != 2:
+        raise ReferenceError("Usage: st_cli [FILE_PATH]")
+        # sys.stderr.write("Usage: st_cli [FILE_PATH]\n")
+        # exit(1)
+
+    try:
+        with open(argv[1]) as f:
+            params = json.load(f)
+    except Exception:
+        raise ReferenceError("Error: ignore params file, " + argv[1])
+        # sys.stderr.write("Error: ignore params file, " + argv[1] + "\n")
+        # exit(1)
+
+    return params
+
+
+def create_signed_transaction(argv):
+    params = get_params(argv)
+    print(params)
