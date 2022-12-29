f40f1b9a138ab4b349e176e49e179b2edfe9fbaa
gldnrt <19574992+gldnrt@users.noreply.github.com>
2022-12-30 08:25:49 +0900

    Improve: 関数の説明追加および不要なコメント削除

--- st_cli/cli.py
+++ st_cli/cli.py
@@ -2,22 +2,22 @@ import json
 
 
 def get_params(argv):
+    """第1引数に指定されたjsonパラメータファイルを読み込む"""
+
     if len(argv) != 2:
         raise ReferenceError("Usage: st_cli [FILE_PATH]")
-        # sys.stderr.write("Usage: st_cli [FILE_PATH]\n")
-        # exit(1)
 
     try:
         with open(argv[1]) as f:
             params = json.load(f)
     except Exception:
         raise ReferenceError("Error: ignore params file, " + argv[1])
-        # sys.stderr.write("Error: ignore params file, " + argv[1] + "\n")
-        # exit(1)
 
     return params
 
 
 def create_signed_transaction(argv):
+    # TODO: オフラインで署名済み送金用トランザクションを作成する
+
     params = get_params(argv)
     print(params)
