b1a9f082c835053dd2ae344aa53bafe8be492e56
gldnrt <19574992+gldnrt@users.noreply.github.com>
2023-01-02 12:31:19 +0900

    Add: 入力パラメータファイルのチェック処理を追加

--- st_cli/cli.py
+++ st_cli/cli.py
@@ -1,19 +1,33 @@
+import os
 import json
+from jsonschema import validate, ValidationError
 from .transaction import Transaction
 
 
-def get_params(argv):
+def check_params(params: object) -> None:
+    parentdir = os.path.dirname(__file__)
+    with open(parentdir + '/schema.json') as file_obj:
+        json_schema = json.load(file_obj)
+
+    try:
+        validate(params, json_schema)
+    except ValidationError as e:
+        raise RuntimeError("Error: ignore params file, " + e.message)
+
+    return
+
+
+def get_params(argv) -> object:
     """第1引数に指定されたjsonパラメータファイルを読み込む"""
 
-    if len(argv) != 2:
+    if 2 != len(argv):
         raise ReferenceError("Usage: st_cli [FILE_PATH]")
 
     try:
         with open(argv[1]) as f:
             params = json.load(f)
-    except Exception:
-        # TODO: エラーメッセージ改善
-        raise ReferenceError("Error: ignore params file, " + argv[1])
+    except Exception as e:
+        raise ReferenceError("Error: ignore params file, " + str(e))
 
     return params
 
@@ -22,6 +36,7 @@ def create_signed_transaction(argv) -> None:
     """オフラインで署名済み送金用トランザクションを作成する"""
 
     params = get_params(argv)
+    check_params(params)
 
     myTransaction = Transaction(params)
     signed_tx = myTransaction.create_signed_tx()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 import pytest
-from st_cli.cli import get_params
+from st_cli.cli import get_params, check_params
 
 
 def test_read_params():
@@ -38,7 +38,7 @@ def test_too_many_args():
     assert str(e.value) == "Usage: st_cli [FILE_PATH]"
 
 
-def test_too_many_args():
+def test_no_existed_file():
     argv = [
         "./signed-transaction-cli/st_cli/__main__.py",
         "./no_existed.json",
@@ -47,7 +47,7 @@ def test_too_many_args():
     with pytest.raises(ReferenceError) as e:
         get_params(argv)
 
-    assert str(e.value) == "Error: ignore params file, ./no_existed.json"
+    assert str(e.value).startswith("Error: ignore params file,")
 
 
 def test_incorrect_json():
@@ -59,6 +59,86 @@ def test_incorrect_json():
     with pytest.raises(ReferenceError) as e:
         get_params(argv)
 
-    assert (
-        str(e.value) == "Error: ignore params file, ./tests/test_params/incorrect.json"
-    )
+    assert str(e.value).startswith("Error: ignore params file, ")
+
+
+class TestCheckParams:
+    '''check_paramsテストクラス'''
+
+    def create_default_params(self) -> str:
+        params = {
+                "network": "regtest",
+                "remittance_amount": 0.0,
+                "transaction_fee": 0.00000,
+                "unspent_transaction": {
+                    "txid": "09af",
+                    "vout": 0,
+                    "value": 0
+                },
+                "address": {
+                    "destination": "azAZ09",
+                    "sender_charge": "90ZAza"
+                }
+            }
+        return params
+
+    def test_check_params(self):
+        params = self.create_default_params()
+        check_params(params)
+
+    @pytest.mark.parametrize("network", ["regtest", "testnet", "mainnet"])
+    def test_check_params_network(self, network):
+        params = self.create_default_params()
+        params["network"] = network
+
+        check_params(params)
+
+    # "params[key]: value"で指定できる値で、
+    # 無効となるキーと値を
+    # (key, value)形式のリストにする
+    ignore_key_value = [
+        ("network", 1),
+        ("network", "ignore"),
+        ("remittance_amount", -1),
+        ("remittance_amount", "0"),
+        ("transaction_fee", -1),
+        ("transaction_fee", "0"),
+    ]
+
+    @pytest.mark.parametrize("key, value", ignore_key_value)
+    def test_check_params_ignore_value(self, key, value):
+        params = self.create_default_params()
+        params[key] = value
+
+        with pytest.raises(RuntimeError):
+            check_params(params)
+
+    # "params[parentkey][key]: value"で指定できる値で、
+    # 無効となるキーと値を
+    # (parentkey, key, value)形式のリストにする
+    ignore_nestedkey_value = [
+        ("unspent_transaction", "txid", 0),
+        ("unspent_transaction", "txid", "g"),
+        ("unspent_transaction", "txid", "0g"),
+        ("unspent_transaction", "txid", "A"),
+        ("unspent_transaction", "vout", -1),
+        ("unspent_transaction", "vout", 0.1),
+        ("unspent_transaction", "vout", "0"),
+        ("address", "destination", 0),
+        ("address", "destination", ")"),
+        ("address", "destination", "0)"),
+        ("address", "sender_charge", 0),
+        ("address", "sender_charge", ")"),
+        ("address", "sender_charge", "0)"),
+    ]
+
+    @pytest.mark.parametrize("parentkey, key, value", ignore_nestedkey_value)
+    def test_check_params_ignore_value_nested_key(self, parentkey, key, value):
+        params = self.create_default_params()
+        params[parentkey][key] = value
+
+        with pytest.raises(RuntimeError) as e:
+            check_params(params)
+
+        print(e)
+
